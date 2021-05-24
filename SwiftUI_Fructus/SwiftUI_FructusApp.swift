8f677e6e1fdeaba85b8e2708dc30f93eb8ef04b1
Han-Saem Park <samiparken@gmail.com>
2021-05-24 11:46:23 +0200

    Initial Commit

---
+++ SwiftUI_Fructus/SwiftUI_FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FructusApp.swift
+//  SwiftUI_Fructus
+//
+//  Created by Sam on 2021-05-24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
