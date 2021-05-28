9d1fc821f5754b1d46f8b143e1a38cc077e006ba
Han-Saem Park <samiparken@gmail.com>
2021-05-28 09:09:34 +0200

    Add ZStack and properies

--- SwiftUI_Fructus/View/FruitCardView.swift
+++ SwiftUI_Fructus/View/FruitCardView.swift
@@ -8,23 +8,27 @@ struct FruitCardView: View {
     //MARK: - BODY
     
     var body: some View {
-        VStack(spacing: 20) {
-            // FRUIT: IMAGE
-            Image("blueberry")
-                .resizable()
-                .scaledToFit()
-                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15),
-                        radius: 0,
-                        x: 6,
-                        y: 8)
-            
-            // FRUIT: TITLE
-            
-            Text("Blueberry")
-            // FRUIT: HEADLINE
-            // BUTTON: START
-            
-        } //: VStack
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15),
+                            radius: 0,
+                            x: 6,
+                            y: 8)
+                
+                // FRUIT: TITLE
+                
+                Text("Blueberry")
+                // FRUIT: HEADLINE
+                // BUTTON: START
+            } //: VStack
+        } //: ZStack
+        .frame(minWidth:0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
     }
 }
 
