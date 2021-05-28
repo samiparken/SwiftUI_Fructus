99f7016eea6edb87f46fb8fb6522fcad50a67099
Han-Saem Park <samiparken@gmail.com>
2021-05-28 09:03:41 +0200

    Add image properties

--- SwiftUI_Fructus/View/FruitCardView.swift
+++ SwiftUI_Fructus/View/FruitCardView.swift
@@ -2,15 +2,29 @@ import SwiftUI
 
 struct FruitCardView: View {
     //MARK: - PROPERTIES
-
+    
     
     
     //MARK: - BODY
     
     var body: some View {
         VStack(spacing: 20) {
+            // FRUIT: IMAGE
+            Image("blueberry")
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15),
+                        radius: 0,
+                        x: 6,
+                        y: 8)
+            
+            // FRUIT: TITLE
+            
             Text("Blueberry")
-        }
+            // FRUIT: HEADLINE
+            // BUTTON: START
+            
+        } //: VStack
     }
 }
 
