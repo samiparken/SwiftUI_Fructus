18065c4768a2207fdf452f53331e73e2bbd6f201
Han-Saem Park <samiparken@gmail.com>
2021-05-29 23:37:49 +0200

    add FruitData.swift

---
+++ SwiftUI_Fructus/Data/FruitData.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+//MARK: - FRUITS DATA
+
+let fruitsData: [Fruit] = [
+    Fruit(
+        title: "Blueberry",
+        headline: "Blueberries are sweet, nutritious and widly popular fruit all over the world.",
+        image: "blueberry",
+        gradientColors: [Color("ColorBlueberryLight"),Color("ColorBlueberryDark")],
+        description: "Blueberries are perennial flowering plants with blue or purple berries.",
+        nutrition: ["240 KJ","9.96 g","0.33 g", "0.74 g", "A, B1", "Calcium, Magnnasium"]
+    ),
+    Fruit(
+        title: "Blueberry",
+        headline: "Blueberries are sweet, nutritious and widly popular fruit all over the world.",
+        image: "blueberry",
+        gradientColors: [Color("ColorBlueberryLight"),Color("ColorBlueberryDark")],
+        description: "Blueberries are perennial flowering plants with blue or purple berries.",
+        nutrition: ["240 KJ","9.96 g","0.33 g", "0.74 g", "A, B1", "Calcium, Magnnasium"]
+    )
+]
