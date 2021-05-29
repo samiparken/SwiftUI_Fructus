0ee68c0d7d119002453b99bcbedf4f2a8752ea02
Han-Saem Park <samiparken@gmail.com>
2021-05-29 23:22:33 +0200

    add OnboardingView

--- SwiftUI_Fructus/App/SwiftUI_FructusApp.swift
+++ SwiftUI_Fructus/App/SwiftUI_FructusApp.swift
@@ -1,17 +1,10 @@
-//
-//  SwiftUI_FructusApp.swift
-//  SwiftUI_Fructus
-//
-//  Created by Sam on 2021-05-24.
-//
-
 import SwiftUI
 
 @main
 struct SwiftUI_FructusApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            OnboardingView()
         }
     }
 }
